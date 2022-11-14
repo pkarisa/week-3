79d584867b607186f05f2a93504405ac90fb3c2f
Pkarisa <faith.pendo@student.moringaschool.com>
2022-11-14 21:53:47 +0300

    update

--- arrays.js
+++ arrays.js
@@ -2,5 +2,7 @@ const myAnimals =["cows","goats","chicken","turkeys"];
 //console.log(myAnimals)
 //myAnimals.push("ducks");
 //console.log(myAnimals)
-myAnimals.splice(1,1);
+//myAnimals.splice(1,1);
+//console.log(myAnimals)
+myAnimals.pop()
 console.log(myAnimals)
